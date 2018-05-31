f30fc21e1a096869d1d3f31e1093e72fa299924e
Adam Urbina <adamurbina@gmail.com>
2018-05-31 14:58:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
     newSentence = []
     sentenceArray = sentence.split
-    sentence.each { |word|
-        newSentence << word.reverse!
+    sentenceArray.each { |word|
+        newSentence << word.reverse
     }
     newSentence.join(" ")
 end
